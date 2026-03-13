60cb463fe0e43756dcc256d12d71611836fd6303
Brian Alexander <brianalexander348@gmail.com>
2026-03-13 21:59:39 +0700

    add folders class

---
+++ src/main/java/com/project/teman_belajar/module/folder/entities/Folders.java
@@ -0,0 +1,36 @@
+package com.project.teman_belajar.module.folder.entities;
+
+import com.project.teman_belajar.module.materials.entities.Materials;
+import com.project.teman_belajar.module.users.entities.Users;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Folders {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String displayId;
+
+    private String name;
+
+    private Date createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @OneToMany(mappedBy = "folders", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Materials> materialsList;
+
+}
