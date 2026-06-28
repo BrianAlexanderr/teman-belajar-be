c10deeafb014298ef4f4f63eb92937b76e8236b4
Brian Alexander <brianalexander348@gmail.com>
2026-06-28 10:58:57 +0700

    add mock to the test

--- src/test/java/com/project/teman_belajar/TemanBelajarApplicationTests.java
+++ src/test/java/com/project/teman_belajar/TemanBelajarApplicationTests.java
@@ -1,12 +1,17 @@
 package com.project.teman_belajar;
 
+import com.project.teman_belajar.module.upload.service.ObjectStorageService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class TemanBelajarApplicationTests {
+
+	@MockitoBean
+	private ObjectStorageService objectStorageService;
 
 	@Test
 	void contextLoads() {
