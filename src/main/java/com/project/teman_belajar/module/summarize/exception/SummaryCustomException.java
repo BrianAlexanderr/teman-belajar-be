cbe90751b5720fff91ae6022438ef6579bbd8456
Brian Alexander <brianalexander348@gmail.com>
2026-07-25 23:03:08 +0700

    add prioritize package isolated custom exception handler

--- src/main/java/com/project/teman_belajar/module/summarize/exception/SummaryCustomException.java
+++ src/main/java/com/project/teman_belajar/module/summarize/exception/SummaryCustomException.java
@@ -3,6 +3,8 @@ package com.project.teman_belajar.module.summarize.exception;
 import com.project.teman_belajar.common.global_exception.dto.ErrorResponse;
 import com.project.teman_belajar.module.summarize.exception.custom_exception.DocumentInvalidException;
 import com.project.teman_belajar.module.summarize.exception.custom_exception.SummaryNotFoundException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice(basePackages = "com.project.teman_belajar.module.summarize")
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SummaryCustomException {
 
     @ExceptionHandler(SummaryNotFoundException.class)
